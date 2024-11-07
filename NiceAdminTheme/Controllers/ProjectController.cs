43b2289c888ef56edeb110637318521bdbcedb92
krishiraj123 <krishirajvansia123@gmail.com>
2024-11-07 18:26:54 +0530

    updated the project mvc

--- NiceAdminTheme/Controllers/ProjectController.cs
+++ NiceAdminTheme/Controllers/ProjectController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
+using NiceAdminTheme.Models;
 
 namespace NiceAdminTheme.Controllers
 {
@@ -43,7 +44,6 @@ namespace NiceAdminTheme.Controllers
                 command.CommandText = "PR_Projects_Delete";
                 command.Parameters.Add("ProjectID", SqlDbType.Int).Value = ProjectID;
                 command.ExecuteNonQuery();
-                return View("ListProjects");
             }
             catch (SqlException e) when (e.Number == 547)
             {
@@ -55,10 +55,56 @@ namespace NiceAdminTheme.Controllers
             }
             return RedirectToAction("ListProjects");
         }
-        
-        public IActionResult ProjectAddEdit()
+
+        public IActionResult ProjectAddEdit(int ProjectID)
         {
-            return View();
+            SqlCommand command = DatabaseConnection();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "PR_Projects_SelectByProjectID";
+            command.Parameters.Add("ProjectID", SqlDbType.Int).Value = ProjectID;
+            SqlDataReader dr = command.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(dr);
+            ProjectModel projectModel = new ProjectModel();
+
+            foreach (DataRow r in table.Rows)
+            {
+                projectModel.ProjectID = Convert.ToInt32(@r["ProjectID"]);
+                projectModel.ProjectName = @r["ProjectName"].ToString();
+                projectModel.Description = @r["Description"].ToString();
+                projectModel.Status = @r["Status"].ToString();
+                projectModel.Budget = Convert.ToInt32(@r["Budget"]);
+                projectModel.CreatedDate = Convert.ToDateTime(@r["CreatedDate"]);
+                projectModel.LastUpdated = Convert.ToDateTime(@r["LastUpdated"]);
+            }
+            
+            return View("ProjectAddEdit", projectModel);
+        }
+        
+        public IActionResult ProjectSave(ProjectModel projectModel)
+        {
+            if (ModelState.IsValid)
+            {
+                SqlCommand command = DatabaseConnection();
+                command.CommandType = CommandType.StoredProcedure;
+                if (projectModel.ProjectID == null)
+                {
+                    command.CommandText = "PR_Projects_Insert";
+                }
+                else
+                {
+                    command.CommandText = "PR_Projects_Update";
+                    command.Parameters.Add("@ProjectID", SqlDbType.Int).Value = projectModel.ProjectID;
+                }
+
+                command.Parameters.Add("ProjectName", SqlDbType.VarChar).Value = projectModel.ProjectName;
+                command.Parameters.Add("Description", SqlDbType.VarChar).Value = projectModel.Description;
+                command.Parameters.Add("Status", SqlDbType.VarChar).Value = projectModel.Status;
+                command.Parameters.Add("Budget", SqlDbType.Int).Value = projectModel.Budget;
+                command.ExecuteNonQuery();
+                return RedirectToAction("ListProjects");
+            }
+            return View("ProjectAddEdit", projectModel);
         }
     }
 }
